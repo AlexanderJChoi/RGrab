367f85b7ac577305d689de93b7f6018e1772581c
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2024-11-20 15:23:22 -0700

    Add Gui components

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ grabbing comments from Reddit
 
 ## Using
 - [PRAW 7.7.1](https://praw.readthedocs.io/en/stable/index.html)
+- [Qt for Python 6.8](https://doc.qt.io/qtforpython-6/index.html)
--- RGrab.py
+++ RGrab.py
@@ -1,35 +1,95 @@
+import sys
+
 import praw
+from PySide6 import QtCore, QtWidgets, QtGui
 
-c_secret=""
-c_id=""
-user_agent=""
-with open(".client_secret", "r") as f:
-	c_secret = f.read().splitlines()[0]
-with open(".client_id", "r") as f:
-    c_id = f.read().splitlines()[0]
-with open(".user_agent", "r") as f:
-    user_agent = f.read().splitlines()[0]
-    
+class RGrabWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        # initialize Qt Widgets
+        self.instructions_text = QtWidgets.QLabel("Paste a link to the reddit thread you wand to scrape comments from: ")
+        self.instructions_text.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.r_link_text = QtWidgets.QLineEdit(self)
+        self.r_link_text.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.row1 = QtWidgets.QHBoxLayout()
+        self.row1.addWidget(self.instructions_text)
+        self.row1.addWidget(self.r_link_text)
+        
+        self.test_text = QtWidgets.QLabel("0", alignment=QtCore.Qt.AlignCenter)
+        
+        self.button = QtWidgets.QPushButton("Select File")
+        self.row2 = QtWidgets.QHBoxLayout()
+        self.row2.addWidget(self.button)
+        self.row2.addWidget(self.test_text)
+        
+        self.scrape_button = QtWidgets.QPushButton("Scrape Reddit Thread")
+        self.row3 = QtWidgets.QHBoxLayout()
+        self.row3.addWidget(self.scrape_button)
+        
+        self.result_text = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
+        self.row4 = QtWidgets.QHBoxLayout()
+        self.row4.addWidget(self.result_text)
+        
+        self.col = QtWidgets.QVBoxLayout(self)
+        self.col.addLayout(self.row1)
+        self.col.addLayout(self.row2)
+        self.col.addLayout(self.row3)
+        self.col.addLayout(self.row4)
+        
+        self.button.clicked.connect(self.select_file)
+        self.scrape_button.clicked.connect(self.scrape)
+        
+        # Initialize PRAW objects
+        c_secret=""
+        c_id=""
+        user_agent=""
+        with open(".client_secret", "r") as f:
+            c_secret = f.read().splitlines()[0]
+        with open(".client_id", "r") as f:
+            c_id = f.read().splitlines()[0]
+        with open(".user_agent", "r") as f:
+            user_agent = f.read().splitlines()[0]
 
-reddit = praw.Reddit(
-	client_id=c_id,
-	client_secret=c_secret,
-	user_agent=user_agent
-)
+        self.reddit = praw.Reddit(
+            client_id=c_id,
+            client_secret=c_secret,
+            user_agent=user_agent
+        )
+        
+    @QtCore.Slot()
+    def select_file(self):
+        filename, file_filter = QtWidgets.QFileDialog.getSaveFileName(self, "Choose a file to save the scraped comments.")
+        self.test_text.setText(filename)
+        
+    @QtCore.Slot()
+    def scrape(self):
+        r_url = self.r_link_text.text()
+        outfile = self.test_text.text()
+        #self.result_text.setText("Working ...") # NOT WORKING
+        #self.scrape_button.setEnabled(False)
+        
+        submission = self.reddit.submission(url=r_url)
+        submission.comments.replace_more(limit=None)
+        counter = 0
+        with open(outfile, "w") as f:
+            for comment in submission.comments.list():
+                f.write(" == BEGIN COMMENT == \n")
+                f.write(comment.body)
+                f.write("\n == END COMMENT == \n")
+                counter+=1
+            f.write(f"Output {counter} comments")
+        
+        #self.scrape_button.setEnabled(True)
+        self.result_text.setText(f"Output {counter} comments to file {outfile}")
 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
 
-url = input("Please enter the link for the reddit comments you want to grab")
-submission = reddit.submission(url=url)
+    widget = RGrabWidget()
+    widget.show()
 
-submission.comments.replace_more(limit=None)
-outfile = input("Please enter the name of the file you want the comments out put to. Use _ in place of spaces.")
-counter = 0
-with open(outfile, "w") as f:
-	for comment in submission.comments.list():
-		f.write(" == BEGIN COMMENT == \n")
-		f.write(comment.body)
-		f.write("\n == END COMMENT == \n")
-		counter+=1
-	f.write(f"Output {counter} comments")
+    sys.exit(app.exec())
 
-print(f"Output {counter} comments to file {outfile}")
