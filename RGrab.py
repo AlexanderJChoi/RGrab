9e8a0413e51aaa8d400f9a83a630c195603c2bd9
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2024-11-22 10:52:08 -0700

    Move scraping into thread and add progress bar

--- RGrab.py
+++ RGrab.py
@@ -1,9 +1,52 @@
+import re
 import sys
 
 import praw
 from PySide6 import QtCore, QtWidgets, QtGui
 
+class Scraper(QtCore.QObject):
+    
+    scrape_progress = QtCore.Signal(int)
+    
+    scrape_finished = QtCore.Signal(int, str)
+    finished = QtCore.Signal()
+    
+    def __init__(self, r_url, outfile, reddit):
+        super().__init__()
+        self.r_url = r_url
+        self.outfile = outfile
+        self.reddit = reddit
+        
+    @QtCore.Slot() 
+    def scrape(self):
+        submission = self.reddit.submission(url=self.r_url)
+        trees = []
+        for tlc in submission.comments:
+            trees.append(praw.models.comment_forest.CommentForest(submission, [tlc]))
+        progress_counter = 0
+        num_trees = len(trees)
+        for t in trees:
+            t.replace_more(limit=None)
+            progress_counter+=1
+            self.scrape_progress.emit(progress_counter * 100 // (num_trees * 2))
+        comment_counter = 0
+        with open(self.outfile, "w") as f:
+            for t in trees:
+                for comment in t.list():
+                    f.write("\n")
+                    f.write(re.sub(r'\s+',' ',comment.body))
+                    f.write("\n")
+                    comment_counter+=1
+                progress_counter+=1
+                self.scrape_progress.emit(progress_counter * 100 // (num_trees * 2))
+            f.write(f"Output {comment_counter} comments")
+        self.scrape_finished.emit(comment_counter, self.outfile)
+        self.finished.emit()
+
+
+
 class RGrabWidget(QtWidgets.QWidget):
+    
     def __init__(self):
         super().__init__()
         
@@ -11,7 +54,7 @@ class RGrabWidget(QtWidgets.QWidget):
         self.instructions_text = QtWidgets.QLabel("Paste a link to the reddit thread you wand to scrape comments from: ")
         self.instructions_text.setAlignment(QtCore.Qt.AlignCenter)
         
-        self.r_link_text = QtWidgets.QLineEdit(self)
+        self.r_link_text = QtWidgets.QLineEdit()
         self.r_link_text.setAlignment(QtCore.Qt.AlignCenter)
         
         self.row1 = QtWidgets.QHBoxLayout()
@@ -30,8 +73,11 @@ class RGrabWidget(QtWidgets.QWidget):
         self.row3.addWidget(self.scrape_button)
         
         self.result_text = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
+        self.progress_bar = QtWidgets.QProgressBar()
+        self.progress_bar.setAlignment(QtCore.Qt.AlignCenter)
         self.row4 = QtWidgets.QHBoxLayout()
         self.row4.addWidget(self.result_text)
+        self.row4.addWidget(self.progress_bar)
         
         self.col = QtWidgets.QVBoxLayout(self)
         self.col.addLayout(self.row1)
@@ -40,7 +86,7 @@ class RGrabWidget(QtWidgets.QWidget):
         self.col.addLayout(self.row4)
         
         self.button.clicked.connect(self.select_file)
-        self.scrape_button.clicked.connect(self.scrape)
+        self.scrape_button.clicked.connect(self.begin_scrape)
         
         # Initialize PRAW objects
         c_secret=""
@@ -59,31 +105,43 @@ class RGrabWidget(QtWidgets.QWidget):
             user_agent=user_agent
         )
         
+        # references to thread and worker
+        # otherwise scoping rules will delete them before they can work!
+        self.scrape_thread = None
+        self.scraper = None
+        
     @QtCore.Slot()
     def select_file(self):
         filename, file_filter = QtWidgets.QFileDialog.getSaveFileName(self, "Choose a file to save the scraped comments.")
         self.test_text.setText(filename)
         
     @QtCore.Slot()
-    def scrape(self):
-        r_url = self.r_link_text.text()
-        outfile = self.test_text.text()
-        #self.result_text.setText("Working ...") # NOT WORKING
-        #self.scrape_button.setEnabled(False)
+    def begin_scrape(self):
+        self.result_text.setText("Working ...")
+        self.scrape_button.setEnabled(False)
         
-        submission = self.reddit.submission(url=r_url)
-        submission.comments.replace_more(limit=None)
-        counter = 0
-        with open(outfile, "w") as f:
-            for comment in submission.comments.list():
-                f.write(" == BEGIN COMMENT == \n")
-                f.write(comment.body)
-                f.write("\n == END COMMENT == \n")
-                counter+=1
-            f.write(f"Output {counter} comments")
+        # Initialize Thread and Worker
+        self.scrape_thread = QtCore.QThread()
+        self.scraper = Scraper(self.r_link_text.text(), self.test_text.text(), self.reddit)
+        self.scraper.moveToThread(self.scrape_thread)
         
-        #self.scrape_button.setEnabled(True)
+        self.scrape_thread.started.connect(self.scraper.scrape)
+        
+        self.scraper.scrape_progress.connect(self.progress_bar.setValue) 
+        
+        self.scraper.finished.connect(self.progress_bar.reset)
+        self.scraper.finished.connect(self.scrape_thread.quit)
+        self.scraper.finished.connect(self.scraper.deleteLater)
+        self.scraper.scrape_finished.connect(self.end_scrape)
+        
+        self.scrape_thread.finished.connect(self.scrape_thread.deleteLater)
+        self.scrape_thread.start()
+        
+    @QtCore.Slot(int, str) 
+    def end_scrape(self, counter, outfile):
         self.result_text.setText(f"Output {counter} comments to file {outfile}")
+        self.scrape_button.setEnabled(True)
+        
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
